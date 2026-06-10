7d69f7d99ff6ab818e7408e6da90f2f8941d76a0
Tman8901 <taquiton@outlook.com>
2026-06-09 20:54:36 -0700

    yipee!

---
+++ README.md
@@ -0,0 +1,7 @@
+# Auto Farmer
+
+## Farming in Minecraft was just made easier.
+
+# ⚠️ DISCLAIMER
+
+**DO NOT** use this on any multiplayer worlds where you are not the owner of. I **do not** condone the use of Macros and cheats on multiplayer servers.
---
+++ src/client/java/auto/farmer/client/AutoFarmerClient.java
@@ -0,0 +1,10 @@
+package auto.farmer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutoFarmerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
